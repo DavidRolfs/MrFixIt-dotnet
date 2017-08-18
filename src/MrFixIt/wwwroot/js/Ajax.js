dac56013e1f3ebea560b1b71aecb719adf42ba08
David Rolfs <davidrolfs15@gmail.com>
2017-08-18 12:41:00 -0700

    user can now update claim using ajax on the index page

---
+++ src/MrFixIt/wwwroot/js/Ajax.js
@@ -0,0 +1,14 @@
+﻿// $(document).ready(function () {
+//    $('.new-claim').click(function (event) {
+//        event.preventDefault();
+//        $.ajax({
+//            type: 'GET',
+//            dataType: 'html',
+//            data: $(this).serialize(),
+//            url: '@Url.Action("Claim")',
+//            success: function (result) {
+//                $('.confirm-claim').html(result);
+//            }
+//        });
+//    });
+//});
